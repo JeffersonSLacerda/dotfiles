17bef27c1e85864ad531595b70240514d88c82e1
JeffersonSLacerda <47826649+JeffersonSLacerda@users.noreply.github.com>
2025-06-06 16:19:50 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
+set -e
 
+# Configurações básicas de shell
 cat bashrc.additions >> ~/.bashrc
-
 cp ./.gitmessage ~
 cp ./.gitconfig ~
 
-# powerline fonts for zsh agnoster theme
-git clone https://github.com/powerline/fonts.git
-cd fonts
-./install.sh
-cd .. && rm -rf fonts
+# Instalação do Oh My Zsh
+RUNZSH=no CHSH=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# oh-my-zsh & plugins
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
-zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
-zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+# Instala plugins essenciais
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Configuração básica do Zsh
 cp ./.zshrc ~
+sed -i '/^ZSH_THEME/c\ZSH_THEME="agnoster"' ~/.zshrc
 
-########################################################################################################################
-#### set agnoster as theme, this came from https://gist.github.com/corentinbettiol/21a6d4e942a0ee58d51acb7996697a88
-#### in vscode settings for devcontainer (not for User or Workspace), Search for terminal.integrated.fontFamily value, and set it to "Roboto Mono for Powerline" (or any of those: https://github.com/powerline/fonts#font-families font families).
-# save current zshrc
-mv ~/.zshrc ~/.zshrc.bak
+# Configuração de fontes (execução em background)
+(
+    git clone https://github.com/powerline/fonts.git /tmp/fonts
+    cd /tmp/fonts
+    ./install.sh
+    cd ..
+    rm -rf /tmp/fonts
+) > /dev/null 2>&1 &
 
-sudo sh -c "$(wget -O- https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)" -- \
-    -t agnoster
-
-# remove newly created zshrc
-rm -f ~/.zshrc
-# restore saved zshrc
-mv ~/.zshrc.bak ~/.zshrc
-# update theme
-sed -i '/^ZSH_THEME/c\ZSH_THEME="agnoster"' ~/.zshrc 
+echo "Configuração básica do ambiente concluída!"
